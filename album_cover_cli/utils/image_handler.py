c2ed171d6e7ea316bc77356bbf173f084de66884
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2023-02-19 10:37:02 -0300

    fix: indentation of image handler

--- album_cover_cli/utils/image_handler.py
+++ album_cover_cli/utils/image_handler.py
@@ -1,39 +1,39 @@
 from PIL import Image, ImageDraw
 
 class ImageHandler:
-  def resize_and_crop_image(image, new_height):
-      width, height = image.size
+  def resize_and_crop_image(self, image, new_height):
+    width, height = image.size
 
-      new_width = int(new_height * width / height)
-      resized_image = image.resize((new_width, new_height))
+    new_width = int(new_height * width / height)
+    resized_image = image.resize((new_width, new_height))
 
-      resized_width, resized_height = resized_image.size
+    resized_width, resized_height = resized_image.size
 
-      left = (resized_width - new_height) / 2
-      top = (resized_height - new_height) / 2
-      right = (resized_width + new_height) / 2
-      bottom = (resized_height + new_height) / 2
+    left = (resized_width - new_height) / 2
+    top = (resized_height - new_height) / 2
+    right = (resized_width + new_height) / 2
+    bottom = (resized_height + new_height) / 2
 
-      return resized_image.crop((left, top, right, bottom))
+    return resized_image.crop((left, top, right, bottom))
 
-  def overlap_images(image1, image2):
-      w1, h1 = image1.size
-      w2, h2 = image2.size
+  def overlap_images(self, image1, image2):
+    w1, h1 = image1.size
+    w2, h2 = image2.size
 
-      x1, y1 = (w2 - w1) // 2, (h2 - h1) // 2
-      x2, y2 = 0, 0
+    x1, y1 = (w2 - w1) // 2, (h2 - h1) // 2
+    x2, y2 = 0, 0
 
-      new_img = Image.new("RGBA", (max(w1, w2), max(w1, w2)), (0, 0, 0, 0))
+    new_img = Image.new("RGBA", (max(w1, w2), max(w1, w2)), (0, 0, 0, 0))
 
-      new_img.paste(image1, (x1, y1))
-      new_img.paste(image2, (x2, y2), image2)
+    new_img.paste(image1, (x1, y1))
+    new_img.paste(image2, (x2, y2), image2)
 
-      return new_img
+    return new_img
 
-  def draw_text_lines(image, font, lines, position, anchor):
+  def draw_text_lines(self, image, font, lines, position, anchor):
     draw = ImageDraw.Draw(image)
 
     for line in lines:
-        height = draw.textsize(line, font=font)[1]
-        draw.text(position, line, font=font, fill='white', anchor=anchor)
-        position = (position[0], position[1] + height)
+      height = draw.textsize(line, font=font)[1]
+      draw.text(position, line, font=font, fill='white', anchor=anchor)
+      position = (position[0], position[1] + height)
