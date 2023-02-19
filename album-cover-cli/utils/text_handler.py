796e1ead295745e04290d40c5d3279737bb099e5
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2023-02-18 23:26:17 -0300

    refactor: text handler

--- album-cover-cli/utils/text_handler.py
+++ album-cover-cli/utils/text_handler.py
@@ -1,17 +1,15 @@
 import textwrap
 
-def calculate_max_chars_per_line(text, font, max_width):
-    max_chars = 0
+class TextHandler:
+  def text_lines(self, text, font, text_max_width):
+    max_chars_per_line = 0
+
     for i in range(len(text)):
-        width = font.getsize(text[:i])[0]
-        if width > max_width:
-            return max_chars
-        max_chars = i
-    return max_chars
+      width = font.getsize(text[:i])[0]
+      if width > text_max_width:
+          break
+      max_chars_per_line = i
 
-def text_lines(text, font, text_max_width):
-  font_width = font.getsize(text)[0]
-  max_chars_per_line = calculate_max_chars_per_line(text, font, text_max_width)
-  lines = textwrap.wrap(text, width=max_chars_per_line, break_long_words=False)
+    lines = textwrap.wrap(text, width=max_chars_per_line, break_long_words=False)
 
-  return lines
+    return lines
